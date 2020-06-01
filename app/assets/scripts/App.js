b14e0cde946722974db8ede1e7b52e1c880cc65b
Alan McCabe <mccabe.alan@gmail.com>
2020-06-01 12:57:26 +0100

    Modal Setup and Complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,12 @@ import "../styles/styles.css";
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+//import Modal from './modules/Modal';
 
 
 let stickyHeader = new StickyHeader();
+
+//new Modal();
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75 );
 new RevealOnScroll(document.querySelectorAll('.testimonial'),  60 );
@@ -12,7 +15,22 @@ new RevealOnScroll(document.querySelectorAll('.testimonial'),  60 );
 
 let mobileMenu = new MobileMenu();
 
+let modal;
 //mobileMenu.events();
+
+document.querySelectorAll('.open-modal').forEach(el => {
+    el.addEventListener('click', e => {
+        e.preventDefault();
+        if(typeof modal == 'undefined'){
+            import(/* webpackChunkName: "modal"*/ './modules/Modal').then(x => {
+                modal = new x.default();
+                setTimeout(() => modal.openTheModal(), 20);
+            }).catch(() => console.log(`Modal JS didn't Load !!!`));
+        }else{
+            modal.openTheModal();
+        }
+    })
+})
 
 if(module.hot){
     module.hot.accept()
